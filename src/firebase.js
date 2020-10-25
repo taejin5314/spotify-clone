ca39d25c723f527b5772e3e0a477c5cadd81394c
taejin <taejin5314@gmail.com>
2020-10-25 14:29:47 -0400

    Cleaning the project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAtwqBLvLhyuzXH5TZQd5m3-YV8uXukDP0",
+    authDomain: "spotify-clone-8b69b.firebaseapp.com",
+    databaseURL: "https://spotify-clone-8b69b.firebaseio.com",
+    projectId: "spotify-clone-8b69b",
+    storageBucket: "spotify-clone-8b69b.appspot.com",
+    messagingSenderId: "438349020512",
+    appId: "1:438349020512:web:e1942565d67e3983e47205",
+    measurementId: "G-5V71DTEQ7V"
+};
