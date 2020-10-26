259399cb2df1def7fcdf1a2ae602e135afd74c59
taejin <taejin5314@gmail.com>
2020-10-26 18:10:47 -0400

    Change logo icon

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './Player.css';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/userSlice';
+import Sidebar from './sidebar/Sidebar';
 
 function Player({ spotify }) {
     const user = useSelector(selectUser);
     return (
         <div className="player">
+            <div className="player__body">
+                {/* Sidebar */}
+                <Sidebar />
+                {/* Body */}
+            </div>
+            {/* Footer */}
             {user?.userInfo.display_name}
         </div>
     )
