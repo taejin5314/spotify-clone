9ddd4d3cdb7d197717b108a1782d7785a4830341
taejin <taejin5314@gmail.com>
2020-10-26 19:42:09 -0400

    Styling: Player

--- src/components/player/sidebar/Sidebar.js
+++ src/components/player/sidebar/Sidebar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Sidebar.css';
 import logo from '../../../images/Spotify_Logo_RGB_White.png';
+import SidebarOption from './sidebarOption/SidebarOption';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src={logo} alt="Spotify Logo" />
+            <SidebarOption title="Home" />
+            <SidebarOption title="Search" />
+            <SidebarOption title="Your Library" />
         </div>
     )
 }
