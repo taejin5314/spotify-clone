ea04892f985215cfa9488cbf5b529f03abc52829
taejin <taejin5314@gmail.com>
2020-10-27 22:30:15 -0400

    Styling: Player > Body > Header

--- src/components/player/body/header/Header.js
+++ src/components/player/body/header/Header.js
@@ -1,8 +1,9 @@
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../../../features/userSlice';
 import './Header.css';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'; import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 function Header() {
     const user = useSelector(selectUser);
@@ -10,12 +11,18 @@ function Header() {
     return (
         <div className="header">
             <div className="header__left">
+                <div className="header__iconButton">
+                    <ArrowBackIosIcon fontSize="small" />
+                </div>
 
+                <div className="header__iconButton">
+                    <ArrowForwardIosIcon fontSize="small" />
+                </div>
             </div>
 
             <div className="header__right">
-                <Avatar src={user?.userInfo?.images[0]?.url} alt="Avatar" />
-                <h4>{user?.userInfo?.display_name}</h4>
+                <button>UPGRADE</button>
+                <Avatar src={user?.userInfo?.images[0]?.url} alt="Avatar" style={{ width: "32px", height: "32px" }} />
             </div>
         </div>
     )
