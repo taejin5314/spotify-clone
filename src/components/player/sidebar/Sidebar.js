653442f09a3fbcf1b99b03ddb20a177cdb0cf01a
taejin <taejin5314@gmail.com>
2020-10-30 17:21:42 -0400

    Sidebar fixing

--- src/components/player/sidebar/Sidebar.js
+++ src/components/player/sidebar/Sidebar.js
@@ -3,27 +3,33 @@ import './Sidebar.css';
 import logo from '../../../images/Spotify_Logo_RGB_White.png';
 import SidebarOption from './sidebarOption/SidebarOption';
 import { ReactComponent as Home } from '../../../images/home.svg';
+import { ReactComponent as HomeSelected } from '../../../images/home_filled.svg'
 import { ReactComponent as Search } from '../../../images/search.svg';
 import { ReactComponent as Library } from '../../../images/library.svg'
 import { useSelector } from 'react-redux';
 import { selectPlaylists } from '../../../features/userSlice';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 function Sidebar({ spotify }) {
     const playlists = useSelector(selectPlaylists);
     const history = useHistory();
+    const location = useLocation();
 
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src={logo} alt="Spotify Logo" onClick={() => history.push('/')} />
             <ul className="sidebar__option">
-                <li className="sidebar__optionList" onClick={() => history.push('/')}>
-                    <SidebarOption Icon={Home} title="Home" />
+                <li className={location.pathname === '/' ? 'sidebar__optionList selected' : 'sidebar__optionList'} onClick={() => history.push('/')}>
+                    {location.pathname === '/' ? (
+                        <SidebarOption Icon={HomeSelected} title="Home" />
+                    ) : (
+                            <SidebarOption Icon={Home} title="Home" />
+                        )}
                 </li>
-                <li className="sidebar__optionList" onClick={() => history.push('/search')}>
+                <li className={location.pathname === '/search' ? 'sidebar__optionList selected' : 'sidebar__optionList'} onClick={() => history.push('/search')}>
                     <SidebarOption Icon={Search} title="Search" />
                 </li>
-                <li className="sidebar__optionList" onClick={() => history.push('/library')}>
+                <li className={location.pathname === '/library' ? 'sidebar__optionList selected' : 'sidebar__optionList'} onClick={() => history.push('/library')}>
                     <SidebarOption Icon={Library} title="Your Library" />
                 </li>
             </ul>
